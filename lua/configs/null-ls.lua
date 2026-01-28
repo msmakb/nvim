670e43fe42a76f43f44c84700240f7003866a1f1
Mohammed Ba Karman <mohammed.saleh@jisr.net>
2026-01-28 13:27:33 +0300

    Update mypy diagnostics with uvx

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -2,6 +2,9 @@ local null_ls = require "null-ls"
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.mypy.with {
+      command = "uvx",
+      args = { "--with", "django-stubs", "mypy" },
+    },
   },
 }
