cac842d1ce491b8cf5e365e8eb4492ff89fa4f7a
Mohammed Ba Karman <mohammed.saleh@jisr.net>
2026-03-14 23:50:50 +0300

    Fix noice lsp on hover issue

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,8 +7,22 @@ return {
         cmdline = { pattern = "^:", icon = "󰘳", lang = "vim" },
       },
     },
+    lsp = {
+      hover = {
+        enabled = false,
+      },
+      signature = {
+        enabled = false,
+      },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = false,
+      },
+    },
   },
   dependencies = {
+    "hrsh7th/nvim-cmp",
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
